b2ad597eeb47a9ffdcb0d1a021b240e09846f093
JamGrif <40271350+JamGrif@users.noreply.github.com>
2019-09-28 15:30:22 +0100

    Main

---
+++ TernaryOperatorTest/TernaryOperatorTest/TernaryOperatorTest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//test
+int main()
+{
+	int Speed = 754;
+	bool SlowDown = Speed > 50 ? true : false;
+	SlowDown == true ? cout << "need to slow down! The speed is " << Speed << endl : cout << "can go faster as your speed is " << Speed << endl;
+}
+
+
