7da763d535aa5f61da2761155cc06c0d3fcc146b
JamGrif <40271350+JamGrif@users.noreply.github.com>
2019-09-30 22:46:10 +0100

    Pointer

--- PointerTest/PointerTest/PointerTest.cpp
+++ PointerTest/PointerTest/PointerTest.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 using namespace std;
 
-/*Pointer is just a variable that holds a memory address <-------
-& = get the address of
-* = get the value at
-*/
+/*Pointer is just a variable that holds a memory address <------- Data type that "points" to another value stored in memory
+& = get the address of / address of operator
+* = get the value at / contents of operator / dereference
 
+Declaring pointer:
+int* p;
+std::string* test;
+float* ff;
+
+Refer to new memory reserved during program execution
+refer and share large data structures without making a copy of the structures
+Used to specify relationships among data - linked lists, tress, graphs
+
+*/
 int main()
 {
-	
-
-
+	int x = 25;
+	int d = 0;
+	int* p; //declared pointer
+	p = &x; //p = address of x
+	d = *p; //d = contents of address p is holding
+	cout << p << endl;
+	cout << d << endl;
 }
 
