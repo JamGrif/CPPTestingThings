58dbbe97f4fec659acf59d2b8df0f6a4ca596a13
JamGrif <40271350+JamGrif@users.noreply.github.com>
2019-09-30 22:14:53 +0100

    Point

---
+++ PointerTest/PointerTest/PointerTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/*Pointer is just a variable that holds a memory address <-------
+& = get the address of
+* = get the value at
+*/
+
+int main()
+{
+	
+
+
+}
+
