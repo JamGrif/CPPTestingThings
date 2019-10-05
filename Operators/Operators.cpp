c496bdf2bbca173f0d11c73b39c7f5b83422c41e
JamGrif <40271350+JamGrif@users.noreply.github.com>
2019-10-05 18:54:27 +0100

    main

---
+++ Operators/Operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	int a = 10;
+	int b = 5;
+	int c = 0;
+
+	
+	std::cout << (a+=b) << std::endl; //a = a + b
+	std::cout << (a=+ b) << std::endl; //a = a + b
+
+	std::cout << (a - b) << std::endl; //a = a - b
+	
+
+
+}
+
