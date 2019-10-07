87e5c3ba023f09514caa9805fac4096068ec1895
JamGrif <40271350+JamGrif@users.noreply.github.com>
2019-10-07 21:45:54 +0100

    Static

--- StaticTest unfinished/StaticTest/StaticTest.cpp
+++ StaticTest unfinished/StaticTest/StaticTest.cpp
@@ -1,72 +1,26 @@
+// Example program
 #include <iostream>
-using namespace std;
+#include <string>
 
-class CourtClass 
-{
+class Animal {
 public:
-
-
-
-
-
-	//Court Dimensions
-	static int m_MinX;
-	static int m_MaxX;
-	static int m_MinY;
-	static int m_MaxY;
-};
-
-class PaddleClass
-{
-public:
-	void PrintDimensions()
+	Animal() 
 	{
-		cout << "P Min x is " << m_MinX << endl;
-		cout << "P Max x is " << m_MaxX << endl;
-		cout << "P Min y is " << m_MinY << endl;
-		cout << "P Max x is " << m_MaxY << endl << endl;
+		Animals++;
+	}
+	void PrintAmount() 
+	{
+		std::cout << "There are " << Animals << " animals." << std::endl;
 	}
 
-	//Court Dimensions
-	static int m_MinX;
-	static int m_MaxX;
-	static int m_MinY;
-	static int m_MaxY;
+	static int Animals;
 };
 
-class BallClass
-{
-public:
-	void PrintDimensions()
-	{
-		cout << "B Min x is " << m_MinX << endl;
-		cout << "B Max x is " << m_MaxX << endl;
-		cout << "B Min y is " << m_MinY << endl;
-		cout << "B Max x is " << m_MaxY << endl;
-	}
 
-	//Court Dimensions
-	static int m_MinX;
-	static int m_MaxX;
-	static int m_MinY;
-	static int m_MaxY;
-};
-
-int CourtClass::m_MinX = 0;
-int CourtClass::m_MaxX = 50;
-int CourtClass::m_MinY = 0;
-int CourtClass::m_MaxY = 25;
 
 int main()
 {
-	PaddleClass paddleobj;
-	BallClass ballobj;
-
-	paddleobj.PrintDimensions();
-	ballobj.PrintDimensions();
+	Animal animal1;
+	animal1.PrintAmount();
 
 }
-
-
-
-
